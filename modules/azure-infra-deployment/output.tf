ac82bfce8bba679faf51770fc9598c40f96c7f5d
Nana Kwadwo <agyemang.nana.b@gmail.com>
2023-11-23 14:45:08 +0000

    marked ssh keys as sensitive

--- modules/azure-infra-deployment/output.tf
+++ modules/azure-infra-deployment/output.tf
@@ -7,9 +7,11 @@ output "vm_user" {
 }
 
 output "ssh_public_key" {
- value = tls_private_key.ssh.public_key_openssh
+  sensitive = true
+  value = tls_private_key.ssh.public_key_openssh
 }
 
 output "ssh_private_key" {
+  sensitive = true
   value = tls_private_key.ssh.private_key_openssh
 }
